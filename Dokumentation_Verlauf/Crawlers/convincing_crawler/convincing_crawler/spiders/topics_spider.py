d2ed4a16da6127f999f0dcfb51ef16fca5e7aa70
trunte <pauline.trunte@adesso.de>
2023-12-17 22:43:52 +0100

    einige Crawler zu Skripten umgeschrieben und ausgearbeitet teilweise Doku vorhanden

--- Dokumentation_Verlauf/Crawlers/convincing_crawler/convincing_crawler/spiders/topics_spider.py
+++ Dokumentation_Verlauf/Crawlers/convincing_crawler/convincing_crawler/spiders/topics_spider.py
@@ -1,40 +1,94 @@
 import scrapy
+from scrapy.crawler import CrawlerRunner
+from scrapy.settings import Settings
+from twisted.internet import reactor
 from collections import Counter
 import re
-#Vorgehensweise wie man die Topics aus unserer copy.py in der Google API nutzen würde
-#In diesem Fall sollte die Google API die URLs der ersten drei Suchergebnisse zu den Topics
-#dem Crawler übergeben, welcher wiederrum die relevanten Parts der Seiten extrahiert und in einer
-#JSON speichert.
-#Das JSON soll dann wiederrum in der copy.py genutzt werden, um die Informationen an die Agents bzw.
-#Personen zu geben, die diese Informationen in der Konversation nutzen
+import logging
+import requests
 
-#Ob es sinnvoll ist, das Scraping der Seiten in unsere Konversationen zu geben, ist nicht sicher.
+
+# Funktion zum Bewerten eines Absatzes anhand der Häufigkeit bestimmter Schlüsselwörter
+def score_paragraph(paragraph, keywords):
+    # Findet alle Wörter im Absatz und zählt ihre Vorkommen
+    words = re.findall(r'\w+', paragraph)
+    word_count = Counter(words)
+    # Berechnet den Gesamtscore, indem die Anzahl jedes Schlüsselwortes summiert wird
+    score = sum(word_count.get(word, 0) for word in keywords)
+    return score
+
+
+# Funktion zur Überprüfung, ob ein Text relevant ist
+def is_relevant(text):
+    # Liste der irrelevanten Schlüsselwörter
+    irrelevant_keywords = ['Newsletter', 'wikiHow', 'Referenzen', 'Autoren', 'anmelden', 'Registriere', 'Mitverfassern',
+                           'aufgerufen']
+    # Überprüft, ob der Text keines der irrelevanten Schlüsselwörter enthält
+    return not any(keyword in text for keyword in irrelevant_keywords)
+
+
+# Spider-Klasse für das Crawlen von Webseiten und das Extrahieren von Informationen
 class TopicsSpider(scrapy.Spider):
     name = 'topics'
-    start_urls = [
-        'https://example.com/page1',  # URL von Google-Suchergebnis 1
-        'https://example.com/page2',  # URL von Google-Suchergebnis 2
-        'https://example.com/page3',  # URL von Google-Suchergebnis 3
-    ]
 
+    # Startet die Anfragen an die URLs, die aus den Google-Suchergebnissen stammen
+    def start_requests(self):
+        # API-Schlüssel, Suchmaschinen-ID und Suchanfrage
+        API_KEY = 'IHR_API_SCHLÜSSEL'
+        SEARCH_ENGINE_ID = 'IHRE_SUCHMASCHINEN_ID'
+        SEARCH_QUERY = 'IHR_SUCHBEGRIFF'
+        # Ableiten der Schlüsselwörter aus der Suchanfrage
+        self.keywords = SEARCH_QUERY.split()
+        # Aufbau der Suchanfrage-URL und Abrufen der Daten
+        search_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={SEARCH_QUERY}"
+        data = requests.get(search_url).json()
+        # Extrahieren der ersten drei URLs aus den Suchergebnissen
+        urls = [item['link'] for item in data.get('items', [])][:3]
+        for url in urls:
+            yield scrapy.Request(url, self.parse)
+
+    # Verarbeitet die Antwort von jeder URL
     def parse(self, response, **kwargs):
+        # Extrahiert Absätze und bewertet sie anhand der Schlüsselwörter
         paragraphs = response.css('p:not(.footer, .byline, .metadata)').extract()
-        scored_paragraphs = [(self.score_paragraph(p), p) for p in paragraphs if self.is_relevant(p)]
+        scored_paragraphs = [(score_paragraph(p, self.keywords), p) for p in paragraphs if is_relevant(p)]
+        # Sortiert die Absätze nach ihrem Score und gibt die Top 10 zurück
         top_paragraphs = sorted(scored_paragraphs, key=lambda x: x[0], reverse=True)[:10]
         for score, paragraph in top_paragraphs:
             yield {'text': paragraph}
 
-    def score_paragraph(self, paragraph):
-        keywords = [#keywords zu den Topics]
-        words = re.findall(r'\w+', paragraph)
-        word_count = Counter(words)
-        score = sum(word_count.get(word, 0) for word in keywords)
-        return score
 
-    def is_relevant(self, text):
-        irrelevant_keywords = ['Newsletter', 'wikiHow', 'Referenzen', 'Autoren', 'anmelden', 'Registriere', 'Mitverfassern', 'aufgerufen']
-        return not any(keyword in text for keyword in irrelevant_keywords)
+# Klasse zur Steuerung des Crawling-Prozesses
+class CrawlerControl:
+    def __init__(self):
+        # Einstellungen für das Speichern der Ergebnisse als JSON
+        self.settings = Settings({
+            'FEED_FORMAT': 'json',
+            'FEED_URI': 'output.json'
+        })
+        # Initialisieren des CrawlerRunners mit diesen Einstellungen
+        self.runner = CrawlerRunner(self.settings)
+        self.crawling = False
 
-# Crawler ausführen mit: scrapy crawl topics
-# Crawler ausführen und Daten im Ordner data in JSON sichern:
-# scrapy crawl topics -o data/topics.json
+    # Startet den Crawler
+    def run(self):
+        if not self.crawling:
+            self.crawling = True
+            # Startet den TopicsSpider und fügt die stop-Methode als Callback hinzu
+            d = self.runner.crawl(TopicsSpider)
+            d.addBoth(self.stop)
+            reactor.run()
+
+    # Stoppt den Crawler und den Twisted-Reactor
+    def stop(self):
+        if self.crawling:
+            self.crawling = False
+            reactor.stop()
+
+
+# Konfiguration des Loggings
+logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+
+# Erstellen und Starten des CrawlerRunners mit der CrawlerControl-Klasse
+crawler_control = CrawlerControl()
+crawler_control.run()
